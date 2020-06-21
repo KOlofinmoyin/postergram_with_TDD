b7fc02b50eab083b30709ae0e20e41e225702bd3
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-06-21 14:25:35 +0100

    generated and migrated 'paperclip' extensions for the post model 'image' variable.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_130050) do
+ActiveRecord::Schema.define(version: 2020_06_21_131829) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
