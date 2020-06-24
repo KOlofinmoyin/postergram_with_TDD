966d05a6e89c5bedf498cf4aec2870c1feb39533
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-06-24 23:55:21 +0100

    created test for Back button feature on 'show' view.

---
+++ spec/features/return_to_index_view_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+
+
+feature 'Returning to ALL posts' do
+  scenario 'can click the Back button on the create post show view' do
+    visit '/'
+    click_link 'New Post'
+    attach_file('Image', "spec/files/images/coffee.jpg")
+    fill_in "Caption", with: 'nom nom nom #coffeetime2'
+    click_button 'Create Post'
+    save_and_open_page
+    expect(page).to have_content('Back')
+  end
+end
